cb17bf10a73cea9cba1dfebadf2b500f6fcda481
Cristian <Cristi@noemail.com>
2023-06-02 17:44:16 +0300

    First draft left projects menu // positioning done // colors and effect to be added later

--- src/components/SmallProjects.jsx
+++ src/components/SmallProjects.jsx
@@ -1,13 +1,22 @@
+import {
+    GiAbdominalArmor,
+    GiBatteredAxe,
+    GiBellShield,
+    GiBestialFangs, GiBloodySword, GiDeathStar, GiGoat
+} from "react-icons/gi";
+
 const SmallProjects = () => {
     return (
-        <div className={"smallProjects"}>
-            <div className={"smallProjects-wrapper"}>
-                <ul className={"smallProjects-list"}>
-                    <li className={"smallProjects-item"}>item 1</li>
-                    <li className={"smallProjects-item"}>item 2</li>
-                    <li className={"smallProjects-item"}>item 3</li>
-                    <li className={"smallProjects-item"}>item 4</li>
-                    <li className={"smallProjects-item"}>item 5</li>
+        <div className={"smallProjects bg-green-800 w-20 ml-[0.5vw] my-5 h-[80vh] flex items-center justify-items-center"}>
+            <div className={"smallProjects-wrapper bg-blue-800 w-[80%] mx-[auto]"}>
+                <ul className={"smallProjects-list bg-indigo-800 flex-col space-y-7  "}>
+                    <li className={"smallProjects-item block bg-purple-300 block p-[5px] py-[10px] flex items-center rounded-[25px]"}> <GiBestialFangs size={40} className={"bg-red-500 mx-[auto]"}/></li>
+                    <li className={"smallProjects-item block p-[5px] py-[10px] flex items-center rounded-[25px]"}> <GiAbdominalArmor size={40} className={"bg-red-500 mx-[auto]"}/> </li>
+                    <li className={"smallProjects-item block p-[5px] py-[10px] flex items-center rounded-[25px]"}> <GiBatteredAxe size={40} className={"bg-red-500 mx-[auto]"}/> </li>
+                    <li className={"smallProjects-item block p-[5px] py-[10px] flex items-center rounded-[25px]"}> <GiBellShield size={40} className={"bg-red-500 mx-[auto]"}/> </li>
+                    <li className={"smallProjects-item block p-[5px] py-[10px] flex items-center rounded-[25px]"}> <GiBloodySword size={40} className={"bg-red-500 mx-[auto]"}/> </li>
+                    <li className={"smallProjects-item bg-purple-300 block p-[5px] py-[10px] flex items-center rounded-[25px]"}> <GiDeathStar size={40} className={"bg-red-500 mx-[auto]"}/> </li>
+                    <li className={"smallProjects-item block p-[5px] py-[10px] flex items-center rounded-[25px]"}> <GiGoat size={40} className={"bg-red-500 mx-[auto]"}/> </li>
                 </ul>
             </div>
         </div>
