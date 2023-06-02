d352a6be62876698314900e158d1d925f03b5606
Cristian <Cristi@noemail.com>
2023-06-02 03:50:31 +0300

    Finished header comp // responsive working with background img // colors to be modified later

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,18 +5,19 @@ const Header = () => {
 
 
     return (
-        <div className={"header"}>
-            <div className={"header-wrapper"}>
-                <div className={"header-left-logo-wrapper"}>
-                    <div className={"left-logo-content"}>
+        <div className={"header bg-gradient-to-r from-cyan-600 to-indigo-700 text-stone-50"}>
+            <div className={"header-wrapper flex flex-row py-[10px]"}>
+                <div className={"header-left-logo-wrapper basis-1/3 pl-[0.5vw]"}>
+                    <div className={"left-logo-content bg-green-300 flex justify-center py-[5px]"}>
                         <p className={"logo"}>X Logo X</p>
                     </div>
                 </div>
-                <div className={"header-right-menu-wrapper"}>
-                    <ul className={"header-right-menu"}>
-                        <li className={"home"}>Home</li>
-                        <li className={"about"}>About</li>
-                        <li className={"contact"}>Contact</li>
+                <div className={"header-right-menu-wrapper basis-2/3 bg-grey-500"}>
+                    <ul className={"header-right-menu flex flex-row-reverse pr-[0.5vw] space-x-[5px]"}>
+                        <li className={"home order-3 px-[5vw] py-[5px] bg-red-500 rounded-full shadow-[0_5px_5px_2px_rgba(0,0,0,0.3)] text-stone-50  drop-shadow"}><p className={"drop-shadow-[1px_1px_1px_rgba(0,0,0,0.9)]"}>Home</p></li>
+                        <li className={"about order-2 px-[5vw] py-[5px] bg-yellow-300 rounded-full shadow-[0_5px_5px_2px_rgba(0,0,0,0.3)] "}>
+                            <p className={"drop-shadow-[1px_1px_1px_rgba(0,0,0,0.9)]"}>About</p></li>
+                        <li className={"contact order-1 px-[5vw] py-[5px] bg-blue-500 rounded-full shadow-[0_5px_5px_2px_rgba(0,0,0,0.3)]"}><p className={"drop-shadow-[1px_1px_1px_rgba(0,0,0,0.9)]"}>Contact</p></li>
                     </ul>
                 </div>
             </div>
